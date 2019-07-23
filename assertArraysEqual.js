a923633084609500445ea7618b112f56d13aaf40
jacksonf21 <jacksonf21@hotmail.com>
2019-07-23 16:33:46 -0400

    Update assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,23 @@
 
-const assertArraysEqual = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
   
     //test array1 array2 values are the same as per index
     let bool = arr1.every((value, index) => {
       return arr1[index] === arr2[index] &&
       arr1.length === arr2.length;
     });
+    return bool;
+};
 
-    //message return
-    if (bool) console.log(`✅️✅️✅️ Array Assertion Passed: ${bool}`);
-    else console.log(`🛑️🛑️🛑️ Array Assertion Failed: ${bool}`);
+const assertArraysEqual = (bool) => {
+  if (bool) console.log(`✅️✅️✅️ Array Assertion Passed: ${bool}`);
+  else console.log(`🛑️🛑️🛑️ Array Assertion Failed: ${bool}`);
 };
 
 //TEST CASES
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,2,'3']);
-assertArraysEqual([1,'2',3],[1,2,3]);
-assertArraysEqual([],[]);
-assertArraysEqual([1,2,3],[4,2,3,1]);
+
+assertArraysEqual(eqArrays([1,2,3],[1,2,3]));
+assertArraysEqual(eqArrays([1,2,3],[1,2,'3']));
+assertArraysEqual(eqArrays([1,'2',3],[1,2,3]));
+assertArraysEqual(eqArrays([],[]));
+assertArraysEqual(eqArrays([1,2,3],[4,2,3,1]));
