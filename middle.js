364b88c25556ba019ace0c379b4a3bb1d849de57
Jackson Fung <jacksonf21@hotmail.com>
2019-07-29 15:19:58 +0000

    headTest change to Mocha Chai testing

--- middle.js
+++ middle.js
@@ -1,23 +1,7 @@
+const assertArraysEqual = require('./assertArraysEqual');
 
-const eqArrays = (arr1, arr2) => {
-  
-  //test array1 array2 values are the same as per index
-  let bool = arr1.every((value, index) => {
-    return arr1[index] === arr2[index] &&
-    arr1.length === arr2.length;
-  });
-  return bool;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2) === true) console.log(`✅️✅️✅️ Array Assertion Passed: "${arr1}" === "${arr2}"`);
-  else if (eqArrays(arr1, arr2) !== true) console.log(`🛑️🛑️🛑️ Array Assertion Failed: "${arr1}" !== "${arr2}"`);
-};
 //i: array
 //o: middle elements of the array (1 if odd) (2 if even)
-  //o: (if len < 2 return '[]');
-
-//
 const middle = (array) => {
   let middleArray = [];
   if (array.length <= 2) {
@@ -32,10 +16,4 @@ const middle = (array) => {
   return middleArray;
 };
 
-//TEST CASES
-assertArraysEqual(middle([1,2]),[]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1,2,3]),[2]);
-assertArraysEqual(middle([1,2,3,4]),[2,3]);
-assertArraysEqual(middle([1,2,3,4,5]),[3]);
-assertArraysEqual(middle([]),[]);
+module.exports = middle;
