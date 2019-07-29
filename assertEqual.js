fc42a5fceea5186c2a7e4c7e76fff2058d0015bf
Jackson Fung <jacksonf21@hotmail.com>
2019-07-29 13:26:31 +0000

    refactor lotide: module reference implemented

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual('Lighthouse Labs', 'Lighthouse Labs');
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(1, 2);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅️✅️✅️ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑️🛑️🛑️ Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]),5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']),'Hello');
-assertEqual(head([]),undefined);
-assertEqual(head([1]),1);
+module.exports = head;
